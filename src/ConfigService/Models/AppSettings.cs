c28c273f60a245ac803384db928c7d6c14d3fa43
SURASAKK <surasak.bcircle@gmail.com>
2018-07-05 11:27:51 +0700

    change AppSettings

--- src/ConfigService/Models/AppSettings.cs
+++ src/ConfigService/Models/AppSettings.cs
@@ -5,10 +5,4 @@ namespace ConfigEditor.Models
         public Project[] Projects { set; get; } = new Project[0];
     }
 
-    public class Project
-    {
-        public string Name { set;get; }
-        public string Path { set; get; }
-        public string[] Patterns { set; get; } = new string[0];
-    }
 }
